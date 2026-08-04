210ae31936066ceccfb6163f507a03ec0fea09e1
João Guilherme Brasil Pichetti <joaogbpichetti@gmail.com>
2026-08-03 22:13:37 -0300

    Criados Logs para os eventos do ciclo de vida de uma activity, para testar

--- OlaMundo/app/src/main/java/br/com/afya/olamundo/MainActivity.kt
+++ OlaMundo/app/src/main/java/br/com/afya/olamundo/MainActivity.kt
@@ -1,12 +1,15 @@
 package br.com.afya.olamundo
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private val TAG = "OlaMundo"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +19,36 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        Log.d(TAG, "onCreate: Activity foi criada e layout inflado.")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i(TAG, "onStart: Activity tornou-se visível ao usuário.")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i(TAG, "onResume: Activity está pronta e em primeiro plano.")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.w(TAG, "onPause: Perdeu o foco. Pausando operações secundárias.")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.w(TAG, "onStop: Não está mais visível na tela.")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG, "onRestart: Usuário retornou à Activity.")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.e(TAG, "onDestroy: Activity sendo destruída e memória liberada.")
     }
 }
