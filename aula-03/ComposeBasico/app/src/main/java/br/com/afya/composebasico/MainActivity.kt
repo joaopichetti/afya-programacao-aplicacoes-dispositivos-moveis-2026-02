42e6a99fec88fb6e7583335871fdea809ce2e9a2
João Guilherme Brasil Pichetti <joaogbpichetti@gmail.com>
2026-08-17 21:29:42 -0300

    Aula 03: Exemplo de empilhamento vertical

--- aula-03/ComposeBasico/app/src/main/java/br/com/afya/composebasico/MainActivity.kt
+++ aula-03/ComposeBasico/app/src/main/java/br/com/afya/composebasico/MainActivity.kt
@@ -4,13 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import br.com.afya.composebasico.ui.theme.ComposeBasicoTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +20,34 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ComposeBasicoTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                CartaoAluno(
+                    modifier = Modifier.padding(20.dp)
+                )
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun CartaoAluno(modifier: Modifier = Modifier) {
+    Column(
         modifier = modifier
-    )
+            .fillMaxWidth()
+            .padding(16.dp),
+    ) {
+        Text(
+            text = "Acadêmico Afya ADS"
+        )
+        Text(
+            text = "4o Período - Dispositivos Móveis"
+        )
+    }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
-fun GreetingPreview() {
+fun CartaoAlunoPreview() {
     ComposeBasicoTheme {
-        Greeting("Android")
+        CartaoAluno(modifier = Modifier.padding(top = 10.dp))
     }
 }
