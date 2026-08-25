509b272ba0bbe47eebbfa8a21f57b82c7b1a71e1
João Guilherme Brasil Pichetti <joaogbpichetti@gmail.com>
2026-08-24 21:24:07 -0300

    Aula 04: Exemplo Variável Comum / Sem Reatividade

--- aula-04/GerenciamentoEstado/app/src/main/java/br/com/afya/gerenciamentoestado/MainActivity.kt
+++ aula-04/GerenciamentoEstado/app/src/main/java/br/com/afya/gerenciamentoestado/MainActivity.kt
@@ -1,17 +1,28 @@
 package br.com.afya.gerenciamentoestado
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import br.com.afya.gerenciamentoestado.ui.theme.GerenciamentoEstadoTheme
+
+private val TAG = "GerenciamentoEstado"
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,10 +31,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             GerenciamentoEstadoTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Contador(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
@@ -31,17 +39,36 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun Contador(modifier: Modifier = Modifier) {
+    var contador = 0
+
+    Column(
         modifier = modifier
-    )
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            "Valor: $contador",
+            fontSize = 28.sp
+        )
+        Spacer(Modifier.height(16.dp))
+        Button(
+            onClick = {
+                contador++
+                Log.d(TAG, "Valor na memória: $contador")
+            }
+        ) {
+            Text("Incrementar")
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, heightDp = 400)
 @Composable
-fun GreetingPreview() {
+fun ContadorPreview() {
     GerenciamentoEstadoTheme {
-        Greeting("Android")
+        Contador()
     }
 }
