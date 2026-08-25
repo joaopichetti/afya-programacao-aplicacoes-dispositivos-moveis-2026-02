55680ebd88318d84029e331ad16f3d083a2e8626
João Guilherme Brasil Pichetti <joaogbpichetti@gmail.com>
2026-08-24 23:02:25 -0300

    Aula 04: Exemplo Scroll na Column

--- aula-04/GerenciamentoEstado/app/src/main/java/br/com/afya/gerenciamentoestado/ContadorCompletoScreen.kt
+++ aula-04/GerenciamentoEstado/app/src/main/java/br/com/afya/gerenciamentoestado/ContadorCompletoScreen.kt
@@ -8,6 +8,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
@@ -185,6 +187,7 @@ fun ContadorCompletoScreen(modifier: Modifier = Modifier) {
             modifier = Modifier
                 .fillMaxWidth()
                 .weight(1f)
+                .verticalScroll(rememberScrollState())
         ) {
             historico.forEach { registro ->
                 ItemHistorico(registro = registro)
